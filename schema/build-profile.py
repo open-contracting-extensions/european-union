b502264f914a4853ff81953bad67e8a7d1c30fc3
James McKinney <james@slashpoundbang.com>
2018-06-28 15:17:51 -0400

    Add schema/build-profile.py

---
+++ schema/build-profile.py
@@ -0,0 +1,14 @@
+# Update this file from a profile with:
+# curl https://raw.githubusercontent.com/open-contracting/standard_profile_template/master/schema/build-profile.py -o schema/build-profile.py  # noqa
+
+import os
+import sys
+
+from ocdsdocumentationsupport import build_profile
+
+basedir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(basedir, '..', 'docs'))
+
+from conf import standard_version, extension_versions  # noqa
+
+build_profile(basedir, standard_version, extension_versions)
