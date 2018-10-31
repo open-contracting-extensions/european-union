dc33f1d682843d6087462e24155cf7941967bca5
James McKinney <james@slashpoundbang.com>
2018-10-31 19:11:23 -0400

    Fix build-profile.py

--- schema/build-profile.py
+++ schema/build-profile.py
@@ -4,11 +4,11 @@
 import os
 import sys
 
-from ocdsdocumentationsupport import build_profile
+from ocdsextensionregistry import build_profile
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(basedir, '..', 'docs'))
 
-from conf import standard_tag, standard_version, extension_versions  # noqa
+from conf import standard_tag, standard_version, extension_versions, schema_base_url  # noqa
 
-build_profile(basedir, standard_tag, extension_versions)
+build_profile(basedir, standard_tag, extension_versions, schema_base_url=schema_base_url)
