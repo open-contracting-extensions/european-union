7d4e7d954139ae08eddd865efefc204a2c964a84
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-29 22:05:03 -0500

    Update files from template

--- include/config.mk
+++ include/config.mk
@@ -15,7 +15,7 @@ LOCALE_DIR=locale
 # Directory in which to build documentation files.
 BUILD_DIR=build
 # Extra build files or directories. (These should match paths in .gitignore.)
-EXTRA_BUILD_FILES=docs/_static/patched
+EXTRA_BUILD_FILES=chromedriver* docs/_static/patched
 # Files that are built and distributed (you may use Bash extended globbing).
 DIST_FILES=schema/profile/release-schema.json schema/profile/codelists schema/patched docs/extensions/!(index).md
 # Directory in which to build .pot files.
--- tests/__init__.py
+++ tests/__init__.py
@@ -6,6 +6,10 @@ test_basic_params = {
     'en': 'OCDS',
 }
 
+test_navigation_params = [
+    ('en', 'Next'),
+]
+
 test_search_params = [
     ('en', r'found \d+ page\(s\) matching'),
 ]
