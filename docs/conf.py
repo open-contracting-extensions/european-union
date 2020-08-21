d8217a611613c304434c80542f28db190778a9a1
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-08-20 20:11:12 -0400

    Upgrade to OCDS 1.1.5

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ html_theme_options = {
 }
 
 # The version of OCDS to patch.
-standard_tag = '1__1__4'
+standard_tag = '1__1__5'
 standard_version = '1.1'
 
 # Where the patched schemas will be deployed.
